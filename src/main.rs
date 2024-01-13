60b2ccfe920689fadd4f803ac1fa4fa2e51d4541
Pavel Perestoronin <pavel.perestoronin@kpn.com>
2024-01-13 21:20:35 +0100

    🎉 Begin a revamp from the scratch

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,6 @@
-use std::iter::FromIterator;
-use std::str::FromStr;
+#![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
 
-use structopt::StructOpt;
+#[global_allocator]
+static ALLOCATOR: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
-use crate::chat_bot::ChatBot;
-use crate::prelude::*;
-use crate::telegram::notifier::Notifier;
-use crate::telegram::Telegram;
-
-pub mod chat_bot;
-pub mod client;
-pub mod logging;
-pub mod marktplaats;
-pub mod math;
-pub mod opts;
-pub mod prelude;
-pub mod redis;
-pub mod result;
-pub mod search_bot;
-pub mod telegram;
-
-#[async_std::main]
-async fn main() -> Result {
-    let opts = opts::Opts::from_args();
-    let _sentry_guard = init_sentry(opts.sentry_dsn);
-
-    logging::init()?;
-    let redis = redis::open(opts.redis_db).await?;
-
-    futures::future::try_join3(
-        search_bot::Bot::new(
-            redis.get_async_std_connection().await?,
-            opts.polling_interval_secs,
-        )
-        .run(),
-        ChatBot::new(
-            Telegram::new(&opts.telegram_token),
-            redis.get_async_std_connection().await?,
-            HashSet::from_iter(opts.allowed_chat_ids),
-        )
-        .run(),
-        Notifier::new(
-            redis.get_async_std_connection().await?,
-            Telegram::new(&opts.telegram_token),
-        )
-        .run(),
-    )
-    .await?;
-
-    Ok(())
-}
-
-fn init_sentry(dsn: Option<String>) -> Option<sentry::ClientInitGuard> {
-    dsn.map(|dsn| {
-        sentry::init(sentry::ClientOptions {
-            dsn: Some(sentry::types::Dsn::from_str(&dsn).unwrap()),
-            release: sentry::release_name!(),
-            attach_stacktrace: true,
-            ..Default::default()
-        })
-    })
-}
+fn main() {}
