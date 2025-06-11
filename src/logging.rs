caee1a738b8e5cfbad58990f5b6440924e45a776
Pavel Perestoronin <eigenein@gmail.com>
2025-06-11 21:34:02 +0200

    🔊 Revert `trim_backtraces`

--- src/logging.rs
+++ src/logging.rs
@@ -22,7 +22,6 @@ impl Logging {
         let sentry_guard = sentry::init(ClientOptions {
             dsn: sentry_dsn.into_dsn()?,
             attach_stacktrace: true,
-            trim_backtraces: false,
             in_app_include: vec![crate_name!()],
             release: Some(Cow::Borrowed(crate_version!())),
             send_default_pii: true,
